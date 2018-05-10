febf345f0fc015b1d15c56891b257d2976594687
wodud95 <39163345+wodud95@users.noreply.github.com>
2018-05-10 23:57:49 +0900

    1
    start

---
+++ app/src/main/java/com/ex/uhs/jyp/shelter_guide/Find_ID_Activity.java
@@ -0,0 +1,13 @@
+package com.ex.uhs.jyp.shelter_guide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Find_ID_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find__id_);
+    }
+}
---
+++ app/src/main/java/com/ex/uhs/jyp/shelter_guide/Find_PW_Activity.java
@@ -0,0 +1,13 @@
+package com.ex.uhs.jyp.shelter_guide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Find_PW_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find__pw_);
+    }
+}
